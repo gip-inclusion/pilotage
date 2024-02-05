4dd2f63071609264935b4439287a659f5d63f497
hellodeloo <hello@hellodeloo.com>
2024-02-05 19:03:24 +0100

    wip: update dashboards for loop

--- pilotage/dashboards/views.py
+++ pilotage/dashboards/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.db.models import OuterRef
 
 from pilotage.dashboards.models import Category, Dashboard
 
 
 def tableaux_de_bord_publics(request):
     dashboards = Dashboard.objects.filter(active=True)
-    categories = Category.objects.all()
+    categories = Category.objects.all().prefetch_related(
+        to_attr="dashboards",
+        queryset=Dashboard.objects.filter(active=True, category=OuterRef("pk")),
+    )
     return render(
         request,
         "dashboards/tableaux_de_bord_publics.html",
