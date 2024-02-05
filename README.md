4755f96ac92ba08b8e1950a74417cb8dcf9bb8bb
hellodeloo <hello@hellodeloo.com>
2024-02-05 12:17:23 +0100

    feat: add dashboards for loop

--- README.md
+++ README.md
@@ -12,10 +12,13 @@ Pour l'accès à l'admin et pour récupérer la db (en sqlite3 pour l'instant), 
 
 ## ToDo
 
-- [ ] Déplacer manage.py et les settings
-- [ ] Créer le role pour les users aux droits limités dans l'admin
-- [ ] Dockeriser la db postgres
-- [ ] Passer la db en postgres (grace à la dockerisation)
-- [ ] Réintégrer les `<iframe>` metabase directement dans le site, sans passer par le C1
-- [ ] Créer une instance CC et héberger le site
-- [ ] Créer le nécessaire pour un déploiement auto (via cmd Makefile ?)
+- Déplacer manage.py et les settings
+- Changer slug admin et mettre la var du slug dans constante .env
+- Fixer le pb local de CORD sur Tally
+- Améliorer [ces boucles imbriquées](https://github.com/hellodeloo/pilotage-django/blob/main/pilotage/templates/dashboards/tableaux_de_bord_publics.html#L50)
+- Dockeriser la db postgres
+- Passer la db en postgres (grace à la dockerisation)
+- Créer le role pour les users aux droits limités dans l'admin
+- Réintégrer les `<iframe>` metabase directement dans le site, sans passer par le C1
+- Créer une instance CC et héberger le site
+- Créer le nécessaire pour un déploiement auto (via cmd Makefile ?)
--- pilotage/pilotage/urls.py
+++ pilotage/pilotage/urls.py
@@ -14,19 +14,32 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from . import views
 
-app_name = 'pilotage'
+app_name = "pilotage"
 urlpatterns = [
-    path('pilotadmin/', admin.site.urls),
-    path('', views.home, name="home"),
-	path('dashboards/', include('dashboards.urls')),
-    path('stats/', views.stats, name="statistiques"),
-    path('accessibilite/', views.accessibilite, name="accessibilite"),
-    path('mentions-legales/', views.mentions_legales, name="mentions_legales"),
-    path('politique-de-confidentialite/', views.politique_de_confidentialite, name="politique_de_confidentialite"),
-    path('inscription-lettre-information/', views.inscription_lettre_information, name="inscription_lettre_information"),
-    path('tableaux-de-bord-prives/', views.tableaux_de_bord_prives, name="tableaux_de_bord_prives"),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("dashboards/", include("dashboards.urls")),
+    path("stats/", views.stats, name="statistiques"),
+    path("accessibilite/", views.accessibilite, name="accessibilite"),
+    path("mentions-legales/", views.mentions_legales, name="mentions_legales"),
+    path(
+        "politique-de-confidentialite/",
+        views.politique_de_confidentialite,
+        name="politique_de_confidentialite",
+    ),
+    path(
+        "inscription-lettre-information/",
+        views.inscription_lettre_information,
+        name="inscription_lettre_information",
+    ),
+    path(
+        "tableaux-de-bord-prives/",
+        views.tableaux_de_bord_prives,
+        name="tableaux_de_bord_prives",
+    ),
 ]
