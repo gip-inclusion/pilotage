fb5a65ca05800de70335db904dd524203024966a
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-06-10 10:36:20 +0200

    env: Put defaults into `.envrc` and simplify docker usability

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -19,3 +19,9 @@ SECURE_HSTS_PRELOAD = False
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 AUTH_PASSWORD_VALIDATORS = []  # Avoid password strength validation in DEV.
+
+DATABASES["default"]["HOST"] = os.getenv("PGHOST", "127.0.0.1")  # noqa: F405
+DATABASES["default"]["PORT"] = os.getenv("PGPORT", "5433")  # noqa: F405
+DATABASES["default"]["NAME"] = os.getenv("PGDATABASE", "pilotage_django")  # noqa: F405
+DATABASES["default"]["USER"] = os.getenv("PGUSER", "postgres")  # noqa: F405
+DATABASES["default"]["PASSWORD"] = os.getenv("PGPASSWORD", "password")  # noqa: F405
