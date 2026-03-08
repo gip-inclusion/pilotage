5468d27248844a88fe0074799bad20172fd9b49f
Romain Sébille <romain.sebille@beta.gouv.fr>
2026-03-08 12:39:49 +0100

    surveys: Make it a loop

--- pilotage/surveys/views.py
+++ pilotage/surveys/views.py
@@ -174,14 +174,13 @@ def tunnel(request, *, survey_name, answer_uid, step):
     previous_step, next_step = get_previous_and_next_step(steps_class, step)
     extra_context = {}
     if step in CommonStep:
-        start_url = request.build_absolute_uri(
-            reverse(
-                "surveys:tunnel",
-                kwargs={"survey_name": survey.name, "answer_uid": answer.uid, "step": CommonStep.INTRODUCTION},
-            )
+        extra_context["start_url"] = reverse(
+            "surveys:tunnel",
+            kwargs={"survey_name": survey.name, "answer_uid": answer.uid, "step": CommonStep.INTRODUCTION},
         )
+        start_absolute_url = request.build_absolute_uri(extra_context["start_url"])
         extra_context["content"] = (getattr(survey, step) or "").replace(
-            "{{START_URL}}", f"[{start_url}]({start_url})"
+            "{{START_URL}}", f"[{start_absolute_url}]({start_absolute_url})"
         )
     else:
         form = get_step_form_class(answer_class, step)(
