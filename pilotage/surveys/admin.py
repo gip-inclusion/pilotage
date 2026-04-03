705511bbd7ba8833b200a313c2595de9c20fe36c
Romain Sébille <romain.sebille@beta.gouv.fr>
2026-04-03 10:41:48 +0200

    surveys: Add an admin link to export filled ESAT answers

--- pilotage/surveys/admin.py
+++ pilotage/surveys/admin.py
@@ -1,10 +1,18 @@
+import csv
+
 from django.contrib import admin
 from django.db.models import Q
-from django.urls import reverse
+from django.http import HttpResponse
+from django.urls import path, reverse
+from django.utils import timezone
 from django.utils.html import format_html
+from django.utils.http import content_disposition_header
 from django.utils.text import capfirst
 
+from pilotage.itoutils.departments import get_department_to_region
 from pilotage.surveys import models
+from pilotage.surveys.forms import ESATAnswerOrganizationForm
+from pilotage.surveys.models import ESATAnswer
 from pilotage.surveys.utils import get_step_form_class, get_steps_informations
 from pilotage.surveys.views import CommonStep, ESATStep
 
@@ -105,11 +113,7 @@ class ESATAnswerAdmin(AnswerAdmin):
     )
     search_fields = ("uid", "finess_num", "esat_siret", "esat_name", "managing_organization_finess")
     list_filter = (FINESSFilledFilter, "esat_dept")
-
-    @admin.display(description="Réponses")
-    def answered_count(self, obj):
-        steps_informations = get_steps_informations(ESATStep, obj, exclude=CommonStep)
-        return f"{steps_informations.total_filled}/{steps_informations.total_fields}"
+    change_list_template = "admin/surveys/change_list.html"
 
     def get_fieldsets(self, request, obj=None):
         steps_fieldsets = []
@@ -128,6 +132,11 @@ class ESATAnswerAdmin(AnswerAdmin):
         links_fieldsets = tuple([("Liens utiles", {"fields": ("start_url",)})])
         return super().get_fieldsets(request, obj) + links_fieldsets + tuple(steps_fieldsets)
 
+    @admin.display(description="Réponses")
+    def answered_count(self, obj):
+        steps_informations = get_steps_informations(ESATStep, obj, exclude=CommonStep)
+        return f"{steps_informations.total_filled}/{steps_informations.total_fields}"
+
     @admin.display(description="URL d'entrée")
     def start_url(self, obj):
         url = reverse(
@@ -135,3 +144,45 @@ class ESATAnswerAdmin(AnswerAdmin):
             kwargs={"survey_name": obj.survey.name, "answer_uid": obj.uid, "step": CommonStep.INTRODUCTION},
         )
         return format_html('<a href="{}">{}</a>', url, CommonStep.INTRODUCTION)
+
+    def get_urls(self):
+        return [
+            path(
+                "download/",
+                self.admin_site.admin_view(self.download_answers),
+                name="download_esat_answers",
+            )
+        ] + super().get_urls()
+
+    def download_answers(self, request):
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={
+                "Content-Disposition": content_disposition_header(
+                    as_attachment=True,
+                    filename=f"esat_answers_{timezone.now():%Y-%m-%d_%H:%M}.csv",
+                ),
+            },
+        )
+
+        data = {}
+        for obj in ESATAnswer.objects.exclude(Q(finess_num=None) | Q(finess_num="")):
+            filled = get_steps_informations(ESATStep, obj, exclude=CommonStep).total_filled
+            # Ignore answers with only FINESS and pre-filled data
+            if filled == len(ESATAnswerOrganizationForm.LOCKED_FIELD_ON_FINESS_DATA) + 1:
+                continue
+
+            if filled > data.get(obj.finess_num, {}).get("Réponses", -1):
+                data[obj.finess_num] = {
+                    "FINESS": obj.finess_num,
+                    "ID": obj.pk,
+                    "Réponses": filled,
+                    "Région": get_department_to_region().get(obj.esat_dept),
+                }
+
+        rows = sorted(data.values(), key=lambda o: (o["Région"], o["FINESS"]))
+
+        writer = csv.DictWriter(response, fieldnames=["Région", "FINESS", "ID", "Réponses"])
+        writer.writeheader()
+        writer.writerows(rows)
+        return response
