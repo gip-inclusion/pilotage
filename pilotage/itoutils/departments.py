37e287bce60591257c120c4c38be8224cfb8cda3
Romain Sébille <romain.sebille@beta.gouv.fr>
2026-04-03 10:41:48 +0200

    surveys: Add an admin link to export filled ESAT answers

--- pilotage/itoutils/departments.py
+++ pilotage/itoutils/departments.py
@@ -1,3 +1,6 @@
+import functools
+
+
 DEPARTMENTS = {
     "01": "Ain",
     "02": "Aisne",
@@ -128,3 +131,36 @@ def department_from_postcode(post_code):
             department = post_code[:2]
 
     return department
+
+
+REGIONS = {
+    "Auvergne-Rhône-Alpes": ["01", "03", "07", "15", "26", "38", "42", "43", "63", "69", "73", "74"],
+    "Bourgogne-Franche-Comté": ["21", "25", "39", "58", "70", "71", "89", "90"],
+    "Bretagne": ["35", "22", "56", "29"],
+    "Centre-Val de Loire": ["18", "28", "36", "37", "41", "45"],
+    "Corse": ["2A", "2B"],
+    "Grand Est": ["08", "10", "51", "52", "54", "55", "57", "67", "68", "88"],
+    "Guadeloupe": ["971"],
+    "Guyane": ["973"],
+    "Hauts-de-France": ["02", "59", "60", "62", "80"],
+    "Île-de-France": ["75", "77", "78", "91", "92", "93", "94", "95"],
+    "La Réunion": ["974"],
+    "Martinique": ["972"],
+    "Mayotte": ["976"],
+    "Normandie": ["14", "27", "50", "61", "76"],
+    "Nouvelle-Aquitaine": ["16", "17", "19", "23", "24", "33", "40", "47", "64", "79", "86", "87"],
+    "Occitanie": ["09", "11", "12", "30", "31", "32", "34", "46", "48", "65", "66", "81", "82"],
+    "Pays de la Loire": ["44", "49", "53", "72", "85"],
+    "Provence-Alpes-Côte d'Azur": ["04", "05", "06", "13", "83", "84"],
+    "Collectivités d'outre-mer": ["975", "977", "978"],
+    "Anciens territoires d'outre-mer": ["984", "986", "987", "988", "989"],
+}
+
+
+@functools.cache
+def get_department_to_region():
+    department_to_region = {}
+    for region, dpts in REGIONS.items():
+        for dpt in dpts:
+            department_to_region[dpt] = region
+    return department_to_region
